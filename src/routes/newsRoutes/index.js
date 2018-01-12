0d220c36c4f8c261af08e07e1282f48880ba08d6
garciadiazjaime <garciadiazjaime@gmail.com>
2018-01-12 07:38:36 -0600

    include mongoose promise

--- src/routes/newsRoutes/index.js
+++ src/routes/newsRoutes/index.js
@@ -1,5 +1,8 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import NewsController from '../../controllers/newsController';
+
+mongoose.Promise = global.Promise;
 
 /*eslint-disable */
 const router = express.Router({mergeParams: true});
