c560daaddcaaa01fc4fca5ce3c63cfdc2eb6f1b6
garciadiazjaime <garciadiazjaime@gmail.com>
2018-01-14 16:44:19 -0600

    enable post endpoint

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 import graphqlHTTP from 'express-graphql'
 
 import newsSchema from './graphql/schema/newsSchema'
+import NewsModel from './model/newsModel'
 
 mongoose.Promise = global.Promise
 
@@ -11,5 +12,26 @@ const router = express.Router()
 router.get('/news', graphqlHTTP(() => ({
   schema: newsSchema
 })))
+
+router.post('/news', (req, res) => {
+  const { data } = req.body
+  const promises = data.map(item => {
+    console.log('item', item)
+    return new NewsModel(item).save()
+  })
+  Promise.all(promises)
+    .then(results => {
+      res.send({
+        status: true,
+        data: results.length
+      })
+    })
+    .catch(error => {
+      res.send({
+        status: false,
+        error
+      })
+    })
+})
 
 export default router
