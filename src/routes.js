d3e91626107289070469b6d004526cc1dfdc846d
garciadiazjaime <garciadiazjaime@gmail.com>
2018-01-13 19:34:34 -0600

    clean

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,6 @@ import mongoose from 'mongoose'
 import graphqlHTTP from 'express-graphql'
 
 import newsSchema from './graphql/schema/newsSchema'
-// import NewsModel from './models/newsModel'
 
 mongoose.Promise = global.Promise
 
@@ -12,21 +11,5 @@ const router = express.Router()
 router.get('/news', graphqlHTTP(() => ({
   schema: newsSchema
 })))
-
-// router.post('/news', (req, res) => {
-//   NewsController.save(req.body)
-//     .then((data) => {
-//       res.json({
-//         status: true,
-//         data,
-//       })
-//     })
-//     .catch((error) => {
-//       res.json({
-//         status: false,
-//         error,
-//       })
-//     })
-// })
 
 export default router
